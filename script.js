7badb490223d777d0a357cc280c101a795915809
Mirza Khalil Ul Rehman <mirzakhalilulrehman123456@gmail.com>
2026-01-23 20:52:27 +0500

    Update html, css js

---
+++ script.js
@@ -0,0 +1,7 @@
+let smb_btn = document.querySelector('.sbm-btn');
+let rating_value = document.querySelectorAll(".star-btn");
+console.log(rating_value);
+smb_btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+})
