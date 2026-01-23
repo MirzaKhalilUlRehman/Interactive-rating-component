a40a6be70bb15de4d1bcb598696a315ba2ec4e1d
Mirza Khalil Ul Rehman <mirzakhalilulrehman123456@gmail.com>
2026-01-23 22:48:36 +0500

    Update html, css js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let smb_btn = document.querySelector('.sbm-btn');
+let img = document.querySelector("img");
 let rating_value = document.querySelectorAll(".star-btn");
+let container = document.querySelector('.container');
 console.log(rating_value);
 smb_btn.addEventListener('click', (e) => {
     e.preventDefault();
+    container.innerHTML = ``;
+    img.setAttribute("src", "asserts/illustration-thank-you.svg");
 
 })
