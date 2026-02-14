7577a4c3dab17034db29e49216b60bcf74329c30
Mirza Khalil Ul Rehman <mirzakhalilulrehman123456@gmail.com>
2026-02-14 15:38:22 +0500

    Update JS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let smb_btn = document.querySelector('.sbm-btn');
 let starBtns = document.querySelectorAll('.star-btn');
 let container = document.querySelector('.container');
+let ratingState = document.querySelector('.rating-state');
 let selectedRating = null;
 
 // Add click event to each star button
@@ -23,13 +24,16 @@ smb_btn.addEventListener('click', (e) => {
         return;
     }
     
+    // Clear container
     container.innerHTML = ``;
     
+    // Add thank you image
     let img = document.createElement('img');
-    img.src = "asserts/illustration-thank-you.svg";
+    img.src = "assets/illustration-thank-you.svg";
     img.classList.add('thank-you-img');
     container.appendChild(img);
 
+    // Add rating badge
     let rating = document.createElement('div');
     let rating_text = document.createElement('p');
     rating_text.innerText = `You selected ${selectedRating} out of 5`;
@@ -37,14 +41,17 @@ smb_btn.addEventListener('click', (e) => {
     rating.appendChild(rating_text);
     container.appendChild(rating);
 
+    // Add thank you heading
     let heading = document.createElement('h1'); 
     heading.innerText = "Thank you!";
     container.appendChild(heading);
 
+    // Add first paragraph
     let para1 = document.createElement('p');
     para1.innerText = "We appreciate you taking the time to give a rating.";
     container.appendChild(para1);
 
+    // Add second paragraph
     let para2 = document.createElement('p');
     para2.innerText = "If you ever need more support, don't hesitate to get in touch!";
     container.appendChild(para2);
