76945b4348432ecc510ade91e4e17f23a44f6f1a
Mirza Khalil Ul Rehman <mirzakhalilulrehman123456@gmail.com>
2026-02-14 15:36:06 +0500

    Update html, css and js

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
 let smb_btn = document.querySelector('.sbm-btn');
-let img = document.querySelector("img");
-let rating_value = document.querySelectorAll(".star-btn");
+let starBtns = document.querySelectorAll('.star-btn');
 let container = document.querySelector('.container');
-console.log(rating_value);
+let selectedRating = null;
+
+// Add click event to each star button
+starBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        // Remove active class from all buttons
+        starBtns.forEach(b => b.classList.remove('active'));
+        // Add active class to clicked button
+        this.classList.add('active');
+        // Store selected value
+        selectedRating = this.getAttribute('data-value');
+    });
+});
+
 smb_btn.addEventListener('click', (e) => {
     e.preventDefault();
+    
+    if (!selectedRating) {
+        alert("Please select a rating before submitting!");
+        return;
+    }
+    
     container.innerHTML = ``;
-    img.setAttribute("src", "asserts/illustration-thank-you.svg");
+    
+    let img = document.createElement('img');
+    img.src = "asserts/illustration-thank-you.svg";
+    img.classList.add('thank-you-img');
+    container.appendChild(img);
 
-})
+    let rating = document.createElement('div');
+    let rating_text = document.createElement('p');
+    rating_text.innerText = `You selected ${selectedRating} out of 5`;
+    rating.classList.add('rating');
+    rating.appendChild(rating_text);
+    container.appendChild(rating);
+
+    let heading = document.createElement('h1'); 
+    heading.innerText = "Thank you!";
+    container.appendChild(heading);
+
+    let para1 = document.createElement('p');
+    para1.innerText = "We appreciate you taking the time to give a rating.";
+    container.appendChild(para1);
+
+    let para2 = document.createElement('p');
+    para2.innerText = "If you ever need more support, don't hesitate to get in touch!";
+    container.appendChild(para2);
+});
